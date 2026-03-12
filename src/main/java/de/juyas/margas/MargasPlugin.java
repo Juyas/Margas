0bbc08b8c90712d46ba32708e07ac5f1822c9f06
Juyas <juyasetgames@gmail.com>
2026-03-12 19:49:18 +0100

    Add initial paper plugin files

---
+++ src/main/java/de/juyas/margas/MargasPlugin.java
@@ -0,0 +1,26 @@
+package de.juyas.margas;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The main class of the plugin.
+ */
+public class MargasPlugin extends JavaPlugin {
+
+    /**
+     * Creates a new instance of the plugin.
+     */
+    public MargasPlugin() {
+        super();
+    }
+
+    @Override
+    public void onEnable() {
+        // Empty
+    }
+
+    @Override
+    public void onDisable() {
+        // Empty
+    }
+}
---
+++ src/main/java/de/juyas/margas/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This package contains the main class and all subpackages of the plugin.
+ */
+@DefaultQualifier(value = NotNull.class, locations = {FIELD, PARAMETER, RETURN})
+package de.juyas.margas;
+
+import org.checkerframework.framework.qual.DefaultQualifier;
+import org.jetbrains.annotations.NotNull;
+
+import static org.checkerframework.framework.qual.TypeUseLocation.FIELD;
+import static org.checkerframework.framework.qual.TypeUseLocation.PARAMETER;
+import static org.checkerframework.framework.qual.TypeUseLocation.RETURN;
