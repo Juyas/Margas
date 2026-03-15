5dac7e4ebfe94713abf585e7fade15ed3560597b
Juyas <juyasetgames@gmail.com>
2026-03-15 14:39:53 +0100

    Add ValueGenerator as supplier that supports exceptions

---
+++ src/main/java/de/juyas/margas/api/config/ValueGenerator.java
@@ -0,0 +1,20 @@
+package de.juyas.margas.api.config;
+
+import de.juyas.margas.api.MargasException;
+
+/**
+ * Interface ValueGenerator to generate a value.
+ * It is essentially a supplier allowed to throw exceptions.
+ */
+@FunctionalInterface
+public interface ValueGenerator<T> {
+
+    /**
+     * Generates a new value of type T.
+     *
+     * @return the generated value
+     * @throws MargasException if the generation fails
+     */
+    T generate() throws MargasException;
+
+}
