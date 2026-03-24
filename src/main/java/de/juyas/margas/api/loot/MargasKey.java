c0d5049e50d707babf34d73cdcf437ba66bee3ad
Juyas <juyasetgames@gmail.com>
2026-03-24 12:11:29 +0100

    Introduce the MargasKeyReader class to parse margas keys from a configuration section

--- src/main/java/de/juyas/margas/api/loot/MargasKey.java
+++ src/main/java/de/juyas/margas/api/loot/MargasKey.java
@@ -2,11 +2,40 @@ package de.juyas.margas.api.loot;
 
 import de.juyas.margas.api.MargasElement;
 import de.juyas.margas.api.MargasType;
+import de.juyas.margas.api.config.TextValue;
+import org.bukkit.Material;
 
 /**
  * Interface MargasKey represents a key configured in the section for {@link MargasType#CHEST_KEY}.
  */
-@FunctionalInterface
 public interface MargasKey extends MargasElement {
+
+    /**
+     * Returns the item type of the key.
+     *
+     * @return the type of the key
+     */
+    Material type();
+
+    /**
+     * Returns the name of the key.
+     *
+     * @return the name of the key
+     */
+    TextValue name();
+
+    /**
+     * Returns the description of the key.
+     *
+     * @return the description of the key
+     */
+    TextValue description();
+
+    /**
+     * Returns if the key is enchanted.
+     *
+     * @return true if the key is enchanted, false otherwise
+     */
+    boolean enchanted();
 
 }
