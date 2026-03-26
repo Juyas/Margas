82df0df8737a5f73353f92a3a69008fb1fdd9193
Juyas <juyasetgames@gmail.com>
2026-03-26 18:18:28 +0100

    Add MargasChestReader to read MargasChest instances

--- src/main/java/de/juyas/margas/api/loot/MargasChest.java
+++ src/main/java/de/juyas/margas/api/loot/MargasChest.java
@@ -1,12 +1,36 @@
 package de.juyas.margas.api.loot;
 
 import de.juyas.margas.api.MargasElement;
+import de.juyas.margas.api.MargasIdentifier;
 import de.juyas.margas.api.MargasType;
+import org.bukkit.Material;
+
+import java.util.List;
 
 /**
  * Interface MargasChest represents a chest configured in the section for {@link MargasType#CHEST}.
  */
-@FunctionalInterface
 public interface MargasChest extends MargasElement {
+
+    /**
+     * Returns the type of the chest.
+     *
+     * @return the type of the chest
+     */
+    Material type();
+
+    /**
+     * Returns the loot table of the chest.
+     *
+     * @return the loot table of the chest
+     */
+    MargasLootTable lootTable();
+
+    /**
+     * Returns the keys to open the chest.
+     *
+     * @return the keys to open the chest
+     */
+    List<MargasIdentifier> keys();
 
 }
---
+++ src/main/java/de/juyas/margas/config/parser/margas/MargasChestReader.java
@@ -0,0 +1,94 @@
+package de.juyas.margas.config.parser.margas;
+
+import de.juyas.margas.api.MargasException;
+import de.juyas.margas.api.MargasIdentifier;
+import de.juyas.margas.api.MargasType;
+import de.juyas.margas.api.config.ConfigSectionReader;
+import de.juyas.margas.api.config.ValueProvider;
+import de.juyas.margas.api.loot.MargasChest;
+import de.juyas.margas.api.loot.MargasLootTable;
+import de.juyas.margas.api.manager.MargasManager;
+import de.juyas.margas.config.DefaultValueProvider;
+import de.juyas.margas.config.parser.EnumReader;
+import de.juyas.margas.config.parser.IdentifierListReader;
+import de.juyas.margas.config.parser.IdentifierReader;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Class MargasChestReader to read a margas chest from a configuration section.
+ */
+public class MargasChestReader implements ConfigSectionReader<MargasChest> {
+
+    /**
+     * The name of the field containing the type of the chest.
+     */
+    private static final String FIELD_CHEST_TYPE = "type";
+
+    /**
+     * The name of the field containing the loot table of the chest.
+     */
+    private static final String FIELD_LOOT_TABLE = "loot";
+
+    /**
+     * The name of the field containing the keys to open the chest.
+     */
+    private static final String FIELD_KEY_LIST = "keys";
+
+    /**
+     * The margas loot table manager.
+     */
+    private final MargasManager<MargasLootTable> lootTableManager;
+
+    /**
+     * Creates a new instance of MargasChestReader.
+     *
+     * @param lootTableManager the margas loot table manager
+     */
+    public MargasChestReader(final MargasManager<MargasLootTable> lootTableManager) {
+        super();
+        this.lootTableManager = lootTableManager;
+    }
+
+    @Override
+    public ValueProvider<MargasChest> read(final ConfigurationSection section, final String path) throws MargasException {
+        if (!section.isConfigurationSection(path)) {
+            throw new MargasException("Invalid margas chest definition in section '%s' at path '%s'.".formatted(section.getCurrentPath(), path));
+        }
+        final ConfigurationSection chestSection = section.getConfigurationSection(path);
+        if (chestSection == null) {
+            throw new MargasException("Missing margas chest definition in section '%s' at path '%s'.".formatted(section.getCurrentPath(), path));
+        }
+
+        final EnumReader<Material> chestTypeReader = new EnumReader<>(Material.class);
+        final IdentifierReader tableReader = new IdentifierReader();
+        final IdentifierListReader keyReader = new IdentifierListReader();
+
+        final ChestIdentifier identifier = new ChestIdentifier(chestSection.getName());
+        final Material chestType = chestTypeReader.read(chestSection, FIELD_CHEST_TYPE);
+        final List<MargasIdentifier> keys = keyReader.read(chestSection, FIELD_KEY_LIST);
+        final MargasIdentifier table = tableReader.read(chestSection, FIELD_LOOT_TABLE);
+        final Optional<ValueProvider<MargasLootTable>> margasLootTable = lootTableManager.get(table);
+        if (margasLootTable.isEmpty()) {
+            throw new MargasException("Invalid loot table definition in section '%s' at path '%s'. Identifier '%s' not found.".formatted(section.getCurrentPath(), path + "." + FIELD_LOOT_TABLE, table.full()));
+        }
+
+        return new DefaultValueProvider<>(new DefaultMargasChest(identifier, chestType, margasLootTable.get().defaultValue(), keys),
+                () -> new DefaultMargasChest(identifier, chestType, margasLootTable.get().generate(), keys), false);
+    }
+
+    private record DefaultMargasChest(MargasIdentifier identifier, Material type, MargasLootTable lootTable,
+                                      List<MargasIdentifier> keys) implements MargasChest {
+    }
+
+    private record ChestIdentifier(String name) implements MargasIdentifier {
+
+        @Override
+        public MargasType type() {
+            return MargasType.CHEST;
+        }
+    }
+}
