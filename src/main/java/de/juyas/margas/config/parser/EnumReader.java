6e0173d76fb3c2474d3ce34339b9f608ec0609fb
Juyas <juyasetgames@gmail.com>
2026-03-31 13:38:12 +0200

    Add support for case-insensitive enum value

--- src/main/java/de/juyas/margas/config/parser/EnumReader.java
+++ src/main/java/de/juyas/margas/config/parser/EnumReader.java
@@ -4,6 +4,8 @@ import de.juyas.margas.api.MargasException;
 import de.juyas.margas.api.config.ConfigValueReader;
 import org.bukkit.configuration.ConfigurationSection;
 
+import java.util.Locale;
+
 /**
  * Class EnumReader to read an enumeration from a configuration section at a given path.
  *
@@ -32,7 +34,7 @@ public class EnumReader<T extends Enum<T>> implements ConfigValueReader<T> {
             throw new MargasException("Enum definition not found at path '%s' in section '%s'.".formatted(path, section.getCurrentPath()));
         }
         try {
-            return Enum.valueOf(enumClass, value);
+            return Enum.valueOf(enumClass, value.toUpperCase(Locale.ROOT));
         } catch (final IllegalArgumentException e) {
             throw new MargasException("Invalid enum definition at path '%s'. '%s' is not a valid enum value for type '%s'".formatted(path, value, enumClass.getSimpleName()), e);
         }
